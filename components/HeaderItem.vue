eae31205f92f2856266fa40b389ef29669a8b1ad
Baptiste Bourzeix <hello@baptistebourzeix.fr>
2022-04-07 21:04:30 +0200

    Header component and logo (WIP)

--- components/HeaderItem.vue
+++ components/HeaderItem.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="relative p-12">
-    BLABLA
+    <NuxtLink class="flex justify-start items-center" to="/">
+      <Logo />
+      <span class="ml-4 text-lg font-dm font-bold text-white">{{ name }}</span>
+    </NuxtLink>
   </div>
 </template>
 
 <script>
+import Logo from '~/assets/svg/logo.svg?inline'
+
 export default {
   name: 'HeaderItem',
+  components: { Logo },
+  data () {
+    return {
+      name: 'Opengento'
+    }
+  },
   head: {
     bodyAttrs: {
-      class: 'bg-zinc-50'
+      class: 'bg-dark-gray'
     },
     htmlAttrs: {
       class: 'dark'
